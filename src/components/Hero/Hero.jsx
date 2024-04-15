868ad48348b49ec134b07fb6ecf2dfb779d8ee70
shaarifalam <alamshaarif@gmail.com>
2024-04-15 14:45:42 +0530

    new morning updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import "./Hero.css";
+import Heroimg from "../../assets/hero.jpg";
 
 const Hero = () => {
   return (
     <div className="hero container">
       <div className="hero-text">
-        <h1>Buy, Sell and Showcase NFT's</h1>
-        <h3>from leading creators and brands</h3>
-        <p>
-          __________________ _____________________ ______________________
-          ______________________ _________________________
-          ______________________ _______________________ _______________________
-          _____________________
-        </p>
+        <h1>Get your work done</h1>
+        <h3>from leading freelancers and agencies.</h3>
+        <p>m</p>
         <button className="btn">Explore featured collections</button>
       </div>
     </div>
