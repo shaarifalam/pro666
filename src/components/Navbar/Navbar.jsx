f14edfab021ef326a7f8747babf0ffea54cacebf
shaarifalam <alamshaarif@gmail.com>
2024-04-24 19:50:55 +0530

    new

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,23 +9,7 @@ const Navbar = () => {
         <img src={logo} alt="" className="logo" />
         <h1>zeunext</h1>
       </div>
-      <ul>
-        <li>
-          <a href="">Home</a>
-        </li>
-        <li>
-          <a href="">Products</a>
-        </li>
-        <li>
-          <a href="">Testimonials</a>
-        </li>
-        <li>
-          <a href="">Reviews</a>
-        </li>
-        <li>
-          <button className="btn">Account</button>
-        </li>
-      </ul>
+      <div className="navlinks"></div>
     </nav>
   );
 };
